51838b03f7dbb90bf87e2acc1d2166da31a5b4fa
Riley110 <reaganroush@gmail.com>
2015-08-26 17:55:39 -0400

    one more thing

--- src/mess/drivers/elwro800.c
+++ src/mess/drivers/elwro800.c
@@ -627,4 +627,4 @@ ROM_END
 /* Driver */
 
 /*    YEAR  NAME       PARENT  COMPAT   MACHINE    INPUT                    INIT COMPANY  FULLNAME      FLAGS */
-COMP( 1986, elwro800,  0,      0,       elwro800,  elwro800, driver_device, 0,   "Elwro", "800 Junior", GAME_NOT_WORKING ) //MACHINE_NOT_WORKING
+COMP( 1986, elwro800,  0,      0,       elwro800,  elwro800, driver_device, 0,   "Elwro", "800 Junior", MACHINE_NOT_WORKING )
